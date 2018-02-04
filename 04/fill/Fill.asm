1bfc7b57d067973dfc52fcf88098a8e9388c4e79
voidSatisfaction <lourie@naver.com>
2018-02-04 21:12:56 +0900

    module4: add assembler codes

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -6,9 +6,118 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+@LOOP
+0;JMP
+
+(LOOP)
+  @KBD
+  D=M
+  @keyin
+  M=D
+  @32
+  D=A
+  @m
+  M=D
+  @256
+  D=A
+  @n
+  M=D
+  @i
+  M=0
+  @k
+  M=0
+  @keyin
+  D=M
+  @SCREEN_BLACK
+  D;JGT
+  @SCREEN_WHITE
+  0;JMP
+
+(SCREEN_BLACK)
+  @j
+  M=0
+  @i
+  D=M
+  @n
+  D=D-M
+  @i
+  M=M+1
+  @LOOP
+  D;JGE
+  @SCREEN_BLACK_INNER
+  0;JMP
+
+(SCREEN_BLACK_INNER)
+  @j
+  D=M
+  @m
+  D=D-M
+  @SCREEN_BLACK
+  D;JGE
+
+  @SCREEN
+  D=A
+  @addr
+  M=D
+
+  @k
+  D=M
+  @addr
+  A=M+D
+  M=-1
+
+  @j
+  M=M+1
+  @k
+  M=M+1
+
+  @SCREEN_BLACK_INNER
+  0;JMP
+
+(SCREEN_WHITE)
+  @j
+  M=0
+  @i
+  D=M
+  @n
+  D=D-M
+  @i
+  M=M+1
+  @LOOP
+  D;JGE
+  @SCREEN_WHITE_INNER
+  0;JMP
+
+(SCREEN_WHITE_INNER)
+  @j
+  D=M
+  @m
+  D=D-M
+  @SCREEN_WHITE
+  D;JGE
+
+  @SCREEN
+  D=A
+  @addr
+  M=D
+
+  @k
+  D=M
+  @addr
+  A=M+D
+  M=0
+
+  @j
+  M=M+1
+  @k
+  M=M+1
+
+  @SCREEN_WHITE_INNER
+  0;JMP
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,46 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@R1
+D=M
+@n
+M=D
+@i
+M=0
+@sum
+M=0
+@SUMLOOP
+0;JMP
+
+(SUMLOOP)
+  @i
+  D=M
+  @n
+  D=D-M
+  @STOP
+  D;JGE
+
+  @R0
+  D=M
+  @sum
+  M=M+D
+
+  @i
+  M=M+1
+
+  @SUMLOOP
+  0;JMP
+
+(STOP)
+  @sum
+  D=M
+  @R2
+  M=D
+
+  @END
+  0;JMP
+
+(END)
+  @END
+  0;JMP
