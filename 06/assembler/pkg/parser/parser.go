72d503f1a5206f40347585e7e2fa114a7f1ed57a
voidSatisfaction <lourie@naver.com>
2018-02-22 02:08:30 +0900

    nand-to-tetris: add assembler of hack-computer

--- 06/assembler/pkg/parser/parser.go
+++ 06/assembler/pkg/parser/parser.go
@@ -1,10 +1,8 @@
 package parser
 
 import (
-	"bufio"
 	"errors"
 	"fmt"
-	"os"
 	"strings"
 )
 
@@ -14,48 +12,130 @@ const (
 	Label = "LABEL"
 )
 
+type symbolTable interface {
+	Set(s string, val int)
+	Get(s string) (int, bool)
+
+	AddOneCursor()
+	GetCursor() int
+	IsSymbolOrLabel(s string) bool
+}
+
 type Parser struct {
-	filePath string
-	result   []struct {
-		commandType string
-		fields      []string
-	}
+	results []result
 }
 
-func New(filePath string) *Parser {
-	return &Parser{filePath: filePath}
+func New() *Parser {
+	return &Parser{}
 }
 
-func (p *Parser) ParseFile() error {
-	f, err := os.Open(p.filePath)
-	s := bufio.NewScanner(f)
-	if err != nil {
-		return err
+func (p *Parser) SetResult(i int, commandType string, fields ...string) {
+	p.results[i] = newResult(commandType, fields...)
+}
+
+func (p *Parser) Results() []result {
+	return p.results
+}
+
+func (p *Parser) addResult(r result) {
+	p.results = append(p.results, r)
+}
+
+type result struct {
+	commandType string
+	fields      []string
+}
+
+func (r result) CommandType() string {
+	return r.commandType
+}
+
+func (r result) Fields() []string {
+	return r.fields
+}
+
+func newResult(commandType string, fields ...string) result {
+	return result{commandType, fields}
+}
+
+func (p *Parser) ParseLine(line string, lineNum int, st symbolTable) (error, int) {
+	lineNum++
+	// check label
+	if checkLabel(line) {
+		lv := getLabelValue(line)
+		st.Set(lv, lineNum)
+		lineNum--
+		return nil, lineNum
 	}
-	defer f.Close()
 
-	for s.Scan() {
-		line := s.Text()
-		// remove strings after //
-		if i := strings.Index(line, "//"); i >= 0 {
-			line = line[:i]
-		}
-		// remove front, and back spaces
-		line = strings.Trim(line, " ")
-		// remove line starting with prefix // or empty line
-		if strings.HasPrefix(line, "//") || line == "" {
-			continue
+	// check C command
+	if checkCCommand(line) {
+		eIndex, semiIndex := strings.Index(line, "="), strings.Index(line, ";")
+
+		// C command with only equal(=)
+		if checkOnlyE(eIndex, semiIndex) {
+			dest, comp := line[:eIndex], line[eIndex+1:]
+			p.addResult(newResult(TypeC, dest, comp, ""))
+			return nil, lineNum
 		}
 
-		// check label
+		// C command with only semicolon(;)
+		if checkOnlySemi(eIndex, semiIndex) {
+			comp, jump := line[:semiIndex], line[semiIndex+1:]
+			p.addResult(newResult(TypeC, "", comp, jump))
+			return nil, lineNum
+		}
 
-		// check C command
-
-		// check A command
-
-		// throw error
-		return errors.New("This is not valid command type " + fmt.Sprintf("%v\n", line))
+		// C command with both equal and semicolon
+		dest, comp, jump := line[:eIndex], line[eIndex+1:semiIndex], line[semiIndex+1:]
+		p.addResult(newResult(TypeC, dest, comp, jump))
+		return nil, lineNum
 	}
 
-	return nil
+	// check A command
+	if checkACommand(line) {
+		rest := line[1:]
+		p.addResult(newResult(TypeA, rest))
+		return nil, lineNum
+	}
+
+	return errors.New(fmt.Sprintf("This is not a valid command type at %d \n %v\n", lineNum, line)), lineNum
+}
+
+func checkLabel(line string) bool {
+	return strings.HasPrefix(line, "(") && strings.HasSuffix(line, ")")
+}
+
+func getLabelValue(line string) string {
+	return line[1 : len(line)-1]
+}
+
+func checkCCommand(line string) bool {
+	eIndex, semiIndex := strings.Index(line, "="), strings.Index(line, ";")
+
+	if eIndex < 0 {
+		return semiIndex >= 0
+	}
+
+	if semiIndex < 0 {
+		return eIndex >= 0
+	}
+
+	return (eIndex >= 0 || semiIndex >= 0) &&
+		(semiIndex >= eIndex+2) &&
+		eIndex >= 1 && semiIndex >= 1 &&
+		!strings.HasSuffix(line, ";") && !strings.HasSuffix(line, "=")
+}
+
+func checkOnlyE(eIndex, semiIndex int) bool {
+	return eIndex >= 1 && semiIndex < 0
+}
+
+func checkOnlySemi(eIndex, semiIndex int) bool {
+	return eIndex < 0 && semiIndex >= 1
+}
+
+func checkACommand(line string) bool {
+	return strings.HasPrefix(line, "@") &&
+		!strings.HasSuffix(line, "@")
 }
